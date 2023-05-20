137b8e9bfbc406741b4a78279588be7ca814628e
boraaziz <boraazizozcan@hotmail.com>
2023-05-21 01:10:14 +0300

    GÜNCELLENDİ
    Eklenenler
    -- Sayımı Bitir butonu eklendi.
    -- CSS Düzenlemeleri yapıldı.
    -- Aynı sayfa içerisinde bir final ekranı tasarlandı.
    -- "Açılan Oy Sayısı" ifadesi, "Açılan Zarf Sayısı" olarak değiştirildi.

--- cbSecim/scripts/sc.js
+++ cbSecim/scripts/sc.js
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<title><?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Sandık numarasını al
-  $sandikNo = $_POST["sandikNo"]; echo $sandikNo; } ?>
- Numaralı Sandık | Dijital Cumhurbaşkanlığı Oy Sayım Çetelesi</title>
-<link rel="stylesheet" type="text/css" href="style/islem.css">
-<link rel="icon" type="image/x-icon" href="img/tr.png">
-<script>
+  // Sayfa üzerinde çift tıklama olayını dinleyen fonksiyon
     function disableZoom(event) {
-      event.preventDefault();
+      event.preventDefault(); // Varsayılan davranışı engelle
     }
+    // Çift tıklama olayını yakala
     document.addEventListener("dblclick", disableZoom);
 function sonlandır(){
    var sonlandirOnay = confirm("Seçimi sonlandırmak istediğinizden emin misiniz? Bu işlemden sonra oylara müdahale edemeyeceksiniz.");
@@ -46,6 +34,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   });
    }
 }
+
+
     // Sandık seçimine geri dön
 function goBack() {
   var onay1 = confirm("Sandık seçimine geri dönmek istediğinizden emin misiniz? Bu işlemden sonra girdiğiniz tüm veriler temizlenecektir.");
@@ -123,88 +113,3 @@ if (onay) {
         document.getElementById("toplam").innerText = toplam;
       }
     }
-</script>
-</head>
-<body>
-<div class="sandik gizle baslik">
-   Sandık Numarası: <strong><?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Sandık numarasını al
-  $sandikNo = $_POST["sandikNo"];
-  if($sandikNo == 2133){
-echo '<script>
-   alert("Merhaba Halil Olcay!");</script>
-   '; } else if($sandikNo == 2134){ echo '
-  <script>alert("Merhaba Sibel!");</script>
-   '; } else if($sandikNo == 2135){ echo '
-  <script>alert("Merhaba Şevval Sude!");</script>
-   '; } else if($sandikNo == 2137){ echo '
-  <script>alert("Merhaba Bora!");</script>
-   '; } echo $sandikNo; } ?></strong>
-</div>
-<header class="baslik">
-<h2>DİJİTAL<br>
- CUMHURBAŞKANLIĞI OY SAYIM ÇETELESİ</h2>
-<h4></h4>
-</header>
-<div class="main degistirMain">
-  <div class="box">
-    <div class="rteFotograf">
-    </div>
-    <span>
-    <h3>Recep Tayyip<br>
-     Erdoğan</h3>
-    </span>
-    <span>
-    <h1 id="rteOy">0</h1>
-    </span>
-    <button class="arti gizle" onclick="oyArttir('rte')">+</button>
-    <p>
-      <button class="eksi gizle" onclick="oyCikart('rte')">-</button>
-    </div>
-    <div class="ortaCizgi degistir">
-    </div>
-    <div class="box">
-      <div class="kkFotograf">
-      </div>
-      <span>
-      <h3>Kemal<br>
-       Kılıçdaroğlu</h3>
-      </span>
-      <span>
-      <h1 id="kkOy">0</h1>
-      </span>
-      <button class="arti gizle" onclick="oyArttir('kk')">+</button>
-      <p>
-        <button class="eksi gizle" onclick="oyCikart('kk')">-</button>
-      </div>
-    </div>
-    <div class="yanCizgi">
-    </div>
-    <div class="gecersizOylar">
-      <h2>Geçersiz Oylar</h2>
-      <h1 id="gecersiz">0</h1>
-      <button class="artiGecersiz gizle" onclick="gecersizArttir()">+</button>
-      <p>
-        <button class="eksiGecersiz gizle" onclick="gecersizCikart()">-</button>
-      </div>
-      <div class="yanCizgi">
-      </div>
-      <div class="gecersizOylar">
-        <h2>Geçerli Oylar</h2>
-        <h1 id="gecerli">0</h1>
-      </div>
-      <div class="yanCizgi">
-      </div>
-      <div class="gecersizOylar">
-        <h2>Açılan Zarf Sayısı</h2>
-        <h1 id="toplam">0</h1>
-      </div>
-      <div class="yanCizgi gizle">
-      </div>
-      <button class="sifirla gizle" onclick="oySifirla()">Tüm Oyları Sıfırla</button>
-      <button class="sifirla gizle" onclick="goBack()">Sandık Seçimine Geri Dön</button>
-      <button class="sifirla2 gizle" onclick="sonlandır()">Sayımı Sonlandır</button>
-      </body>
-      <footer></footer>
-      </html>
